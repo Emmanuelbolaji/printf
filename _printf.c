dbac6a89676bf5570ba72473cb70d908c4fb373c
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-11-12 06:59:53 -0800

    print

--- _printf.c
+++ _printf.c
@@ -1,45 +1,37 @@
 #include "main.h"
+/**
+ * _printf - implements a costume printf
+ * @format: a character string
+ * Return: the number of character printed
+ */
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	for (; *format; format++)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					putchar(va_arg(args, int));
-                    i++;
-                    break;
-                case 's':
-                    {
-                        const char *string = va_arg(args, const char *);
-                        for (;*string; string++) {
-                            putchar(*string);
-                            i++;
-                        }
-                    }
-                    break;
-                case '%':
-                   putchar('%');
-                    i++;
-                    break;
-                default:
-                  
-                    break;
+    for (; *format; format++) {
+        if (*format == '%') {
+            format++;
+            if (*format == 'c') {
+                putchar(va_arg(args, int));
+            } else if (*format == 's') {
+                const char *string = va_arg(args, const char *);
+                for (; *string; string++) {
+                    putchar(*string);
+                }
+            } else if (*format == '%') {
+                putchar('%');
             }
+            count++;
         } else {
             putchar(*format);
-            i++;
-      }
+            count++;
+        }
     }
-	va_end(args);
-	return i;
+
+    va_end(args);
+    return count;
 }
